a7272a7db9a5d078e1eecd8003ae6012e916ed18
Denis <tudow40@gmail.com>
2025-06-11 12:14:56 +0300

    Updates

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import SequenceAlignment from '@/components/SequenceAlignment';
-
-function App() {
-  return <SequenceAlignment />;
-}
-
-export default App;
--- src/components/SequenceAlignment/components/AminoAcidChar/constants.ts
+++
@@ -1,5 +0,0 @@
-export const CHAR_FONT_SIZE = 20;
-export const CHAR_PADDING = '4px';
-export const CHAR_BORDER_RADIUS = '2px';
-export const CHAR_FONT_WEIGHT = 'bold';
-export const CHAR_FONT_FAMILY = 'monospace';
